336b6727c839bd35c890d0bff263c663b6961c69
Thuan Nguyen <thuannh0206@gmail.com>
2019-11-14 11:06:35 +0700

    [INTERNAL] Change the code based on the feedback

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // import { sayHello } from 'say_hello.js';
-var rotator = require('./rotator/rotator.js');
-rotator.start();
+// var rotator = require('./rotator/rotator.js');
+
+import { startRotating } from './rotator/rotator.js';
+var A = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [9, 6, 2, 4, 5, 7, 1, 4, 7, 1],
+    [3, 4, 5, 3, 5, 5, 2, 6, 5, 5],
+    [1, 5, 6, 7, 7, 9, 5, 1, 7, 4],
+    [6, 6, 1, 6, 8, 8, 9, 9, 1, 9],
+    [1, 5, 3, 4, 8, 3, 8, 3, 8, 2],
+    [1, 5, 3, 4, 8, 3, 8, 3, 8, 2],
+    [1, 5, 3, 4, 8, 3, 8, 3, 8, 2],
+    [1, 5, 3, 4, 8, 3, 8, 3, 8, 2],
+    [1, 5, 3, 4, 8, 3, 8, 3, 8, 2]
+],
+b=3;
+startRotating(A, b);
--- test/test.js
+++ test/test.js
@@ -1,39 +1,13 @@
-var assert = require('assert');
-var rotator = require('../rotator/rotator');
-var expect = require('chai').expect;
+import assert from 'assert';
+import { startRotating } from '../rotator/rotator.js';
 
-    describe('# It is testing the rotator module', function () {
-        it("Testing the initArrayB function", function () {
-            console.log(rotator);
+describe('# It is testing the startRotating module', function () {
+    it("Testing the startRotating function", async function () {
             var aMockArray=[
                 [1,1],
                 [2,2]
             ];
-            var aArrayB=rotator.initArrayB(aMockArray);
-            assert.equal(aArrayB.length, 2);
-        });
-        it("Testing the readPictureFromFile function", function () {
-            var iTimes=rotator.readPictureFromFile().iTimes;
-            assert.equal(iTimes, 3);
-        });
-        it("Testing the logTable function", function () {
-            var aMockArray = [
-                [1, 1],
-                [2, 2]
-            ];
-            var result = rotator.logTable(aMockArray);
-            assert.ok(rotator, "rotator.logTable ran well");
-        });
-        it("Testing the start function", function () {
-            var result = rotator.start();
-            assert.ok(result, "rotator.start ran well");
-        });
-        it("Testing the rotate function", function () {
-            var aMockArray = [
-                [1, 1],
-                [2, 2]
-            ];
-            var result = rotator.rotate(aMockArray, 1);
-            assert.equal(result[0][0], 2);
+        var aArrayB = await startRotating(aMockArray, 2);
+            assert.equal(aArrayB[0][0], 2);
         });
     });
